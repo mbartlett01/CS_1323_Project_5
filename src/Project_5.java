47913d56393884d83e492193cb4a9b09a7f69f41
mbartlett01 <67392004+mbartlett01@users.noreply.github.com>
2020-07-12 12:37:15 -0500

    Initial Commit

---
+++ src/Project_5.java
@@ -0,0 +1,55 @@
+// CS 1323 Summer 2020
+// This is a template file, you may use this for your code.
+
+
+
+public class Project_5 {
+
+	/**
+	 * do not change these values and make sure to use
+	 * black and white instead of their number in the code
+	**/
+	
+	public static final int BLACK = 0;
+	public static final int WHITE = 255;
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		// call all the methods
+	}
+
+	public static int[][] createVerticalStripes(int height, int width, int stripeWidth) {
+
+		// in this method you have to write a nested loop to create vertical stripe pattern
+
+		return null; // change null to the actual array
+	}
+
+	public static int[][] createHorizontalStripes(int height, int width, int stripeWidth) {
+
+		// in this method you have to write a nested loop to create horizontal stripe pattern
+
+		return null; // change null to the actual array
+	}
+
+	public static int[][] createCheckerboard(int height, int width, int stripeWidth) {
+
+		// in this method you have to write a nested loop to create checkerboard stripe pattern
+		// keep in mind, the checkerboard should have alternate black & white stripe both
+		// horizontally and vertically
+
+		return null;
+	}
+
+	public static int[][] createDiagonalStripes(int height, int width, int stripeWidth) {
+
+		// in this method you have to write a nested loop to create diagonal stripe pattern
+
+		return null;
+	}
+
+	public static void saveImage(String filename, int[][] image) throws FileNotFoundException {
+
+		// in this methiod you need to read in the array and write it to the appropriate file name
+	}
+}
